e0c89c45e8ced4ea1d5e1cd8e1adeaf758816b93
hieult <hieuletrung307@gmail.com>
2020-11-03 00:13:19 +0700

    Update Zeanni_model.php

--- application/models/Zeanni_model.php
+++ application/models/Zeanni_model.php
@@ -1723,7 +1723,7 @@ class Zeanni_model extends CI_Model
                 $arr=array();
                 if($time=='1t'){
                     $arrDate = array();
-                    for($i=0;$i<=6;$i++)
+                    for($i=0;$i<=6;$i++){
                         $date = date_add(date_create($week_start),date_interval_create_from_date_string( $i." days"));
                         $date = date_format($date,"Y-m-d");
                         $arrDate[$i] = $date;
