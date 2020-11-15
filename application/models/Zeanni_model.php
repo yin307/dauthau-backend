dffb495837a84fe829fd0fe4ad4bd95bf4142320
hieult <hieuletrung307@gmail.com>
2020-11-15 15:23:59 +0700

    Update Zeanni_model.php

--- application/models/Zeanni_model.php
+++ application/models/Zeanni_model.php
@@ -1257,6 +1257,10 @@ class Zeanni_model extends CI_Model
         //     from \"TBL_BIDINGS\" a1 
         //     where a1.\"NOTI_TYPE\" = '1' 
         //     group by a1.\"BID_PACKAGE_CODE\") a2 on a2.\"BIDING_ID\"=a1.\"BIDING_ID\"
+
+
+        // bo noti_type=1
+
         $sql = "SELECT * FROM ( 
             SELECT a.*,NVL(a.\"a1-zn-PACKAGE_NAME1\",a2.PACKAGE_NAME) as \"a1-zn-PACKAGE_NAME\", rownum r__ 
             FROM ( 
