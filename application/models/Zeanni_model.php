75f4b60974b0b4719383cf724434f7ece9cf9938
hieult <hieuletrung307@gmail.com>
2020-11-15 14:17:23 +0700

    Update Zeanni_model.php

--- application/models/Zeanni_model.php
+++ application/models/Zeanni_model.php
@@ -1184,7 +1184,8 @@ class Zeanni_model extends CI_Model
                 a1.\"SUBMISSION_PLACE\" as \"a1-zn-SUBMISSION_PLACE\",  a1.\"BID_TYPE\" as \"a1-zn-BID_TYPE\"  ,
                 a3.\"BIDING_ID\" as \"a3-zn-BIDING_ID\",a1.\"COUNT_SUB\",a1.STAGE_BIDDING,a1.\"PROCURING_CODE\",
                 a1.\"PRE_DOC_PLACE\",
-                a1.\"PRE_OPEN_PLACE\"
+                a1.\"PRE_OPEN_PLACE\",
+                a1.\"EXCUTE_CONTRACT_TIME\"
             from \"TBL_BID_PACKAGES\" a1 
             left join \"TBL_PROCURINGS\" a2  on a2.\"PROCURING_CODE\" = a1.\"PROCURING_CODE\" 
             left join \"TBL_BIDINGS\" a3 on a1.\"BID_PACKAGE_CODE\" = a3.\"BID_BID_PACKAGE_CODE\" and a3.\"NOTI_TYPE\" != 1
