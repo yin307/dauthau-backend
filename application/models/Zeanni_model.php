744dbb53c6f8a14863737366189bb8b64bb0c728
hieult <hieuletrung307@gmail.com>
2021-03-07 13:51:26 +0700

    Update Zeanni_model.php

--- application/models/Zeanni_model.php
+++ application/models/Zeanni_model.php
@@ -1070,6 +1070,7 @@ class Zeanni_model extends CI_Model
             ) a 
             WHERE rownum < ((" . $page . " * 100) + 1 ) 
         ) WHERE r__ >= (((" . $page . "-1) * 100) + 1)";
+        echo $sql;
         $query = $this->db->query($sql);
         $data =  $query->result_array();
         return $data;
